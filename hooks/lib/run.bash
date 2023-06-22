726e622fc40ecad2d3fb775f1307ee3f391d2da7
Ben Chatelain <ben@octop.ad>
2023-06-22 10:54:58 -0600

    🐛 cd into root before build

--- hooks/lib/run.bash
+++ hooks/lib/run.bash
@@ -7,8 +7,11 @@ cmd_path="${script_dir}/../../bin/run"
 # This turns on debugging for monofo, which is important to see what's going on
 export DEBUG="monofo:*"
 
-# Ensure typescript has been built
-yarn build
+(
+    # Ensure typescript has been built
+    cd "${script_dir}/../.."
+    yarn build
+)
 
 function monofo() {
     echo "$cmd_path ${*}"
