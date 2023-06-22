4f8424db0721116d185d8e1e29782e1b08cfa6f8
Ben Chatelain <ben@octop.ad>
2023-06-22 12:38:03 -0600

    👔 Force monofo to only look at the PR's target/base branch

--- hooks/lib/run.bash
+++ hooks/lib/run.bash
@@ -7,6 +7,10 @@ cmd_path="${script_dir}/../../bin/run"
 # This turns on debugging for monofo, which is important to see what's going on
 export DEBUG="monofo:*"
 
+# Force monofo to only look at the PR's target/base branch
+export MONOFO_DEFAULT_BRANCH=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
+export MONOFO_INTEGRATION_BRANCH=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
+
 (
     # Ensure typescript has been built
     cd "${script_dir}/../.."
